7c4c7f137d1ff59bbd991206907548fb7225f042
Abdullah Bin Masood <abm8656@gmail.com>
2023-10-04 01:43:12 +0500

    Meet and Zoom bots with locators added
    This commit contains the updated code for meet bot and zoom bot (meet bot is named as bot.py)

--- bot.py
+++ bot.py
@@ -16,41 +16,45 @@ import random
 import sys
 import json
 from dotenv import load_dotenv
+from locators import Locators
 
 options = uc.ChromeOptions()
 options.add_argument('--no-sandbox')
 options.add_argument('--password-store=basic')
+options.add_argument('--headless')  # Use headless Chrome
+options.add_argument("--use-fake-ui-for-media-stream")  # Allow access to microphone and camera without prompting
 options.add_experimental_option(
-        "prefs",
-        {
-            "credentials_enable_service": False,
-            "profile.password_manager_enabled": False,
-        },
-    )
+    "prefs",
+    {
+        "credentials_enable_service": False,
+        "profile.password_manager_enabled": False,
+    },
+)
 params = {
     "behavior": "allow",
-    "downloadPath": os.getcwd(), # Or whatever path you want
+    "downloadPath": os.getcwd(),  # Or whatever path you want
 }
 
 print("Creating driver...")
-driver = uc.Chrome(options=options, use_subprocess=True, version_main=114)
+driver = uc.Chrome(options=options, use_subprocess=True)
 driver.execute_cdp_cmd("Page.setDownloadBehavior", params)
 
-
-ALONE_TIMEOUT = 60*5 # 5 mins
-LOBBY_TIMEOUT = 60*10 # 10 mins :)
-MAX_LENGTH = 2*60*60 # 2 hours
-
+ALONE_TIMEOUT = 60 * 5  # 5 mins
+LOBBY_TIMEOUT = 60 * 10  # 10 mins :)
+MAX_LENGTH = 2 * 60 * 60  # 2 hours
 
 load_dotenv()
 status_url = os.getenv("STATUS_URL")
 uuid = os.getenv("UUID")
+
+
 def send_status(msg):
     try:
         if msg:
-            requests.post(status_url+str(uuid)+"/", data={"uuid": uuid, "status": msg})
+            requests.post(status_url + str(uuid) + "/", data={"uuid": uuid, "status": msg})
     except:
         pass
+
 
 class EnhancedActionChains(ActionChains):
     def send_keys_1by1(self, keys_to_send, time_s=0.2):
@@ -63,17 +67,21 @@ class EnhancedActionChains(ActionChains):
             self.w3c_actions.key_action.pause(time_s)
 
         return self
+
+
 def waitwithss(sec):
     for i in range(0, sec):
         time.sleep(1)
-        driver.save_screenshot('live.png') # For debugging
+        driver.save_screenshot('live.png')  # For debugging
+
 
 def stopStream():
     driver.execute_script("""
         var stopelm = document.createElement("p");
         stopelm.setAttribute("id","stoprecord");
         document.querySelector('body').appendChild(stopelm)
-    """) 
+    """)
+
 
 def check_if_detected(driver):
     try:
@@ -96,78 +104,74 @@ def timed_out(driver, start_time):
 
 def check_if_in_call(driver):
     try:
-        driver.find_element(By.XPATH, "/html/body/div[1]/c-wiz/div[1]/div/div[14]/div[3]/div[10]")
+        driver.find_element(By.XPATH, Locators.MEET_CHECK_IF_IN_CALL_LOCATOR_ONE)
         return True
     except Exception as e:
         pass
     try:
-        driver.find_element(By.XPATH, "/html/body/div[1]/c-wiz/div[1]/div/div[13]/div[3]/div[10]")
+        driver.find_element(By.XPATH, Locators.MEET_CHECK_IF_IN_CALL_LOCATOR_TWO)
         return True
     except Exception as e:
         pass
     try:
-        driver.find_element(By.XPATH, "/html/body/div[1]/c-wiz/div[1]/div/div[13]/div[3]/div[11]")
+        driver.find_element(By.XPATH, Locators.MEET_CHECK_IF_IN_CALL_LOCATOR_THREE)
         return True
     except Exception as e:
         pass
     try:
-        driver.find_element(By.XPATH, "/html/body/div[1]/c-wiz/div[1]/div/div[14]/div[3]/div[11]")
+        driver.find_element(By.XPATH, Locators.MEET_CHECK_IF_IN_CALL_LOCATOR_FOUR)
         return True
     except Exception as e:
         pass
     return False
-    
+
+
 def is_kicked(driver):
     try:
         kicked = driver.find_element(
-            By.CSS_SELECTOR, "#ow3 > div > div > h1.roSPhc")
+            By.CSS_SELECTOR, Locators.MEET_KICKED_OUT_MODAL)
         return True
     except:
         pass
     return False
 
+
 def alone_in_room(driver, start_time):
     try:
-        participants = driver.find_elements(
-            By.CSS_SELECTOR, "#ow3 > div.T4LgNb > div > div:nth-child(13) > div.crqnQb > div.UnvNgf.Sdwpn.P9KVBf.IYIJAc.BIBiNe > div.jsNRx > div.fXLZ2 > div > div > div:nth-child(2) > div > div")
-        if participants and participants[0].text == "1"  and time.time() - start_time > ALONE_TIMEOUT:
+        participants = driver.find_elements(By.CSS_SELECTOR, Locators.MEET_ALONE_IN_ROOM_LOCATOR)
+        if participants and participants[0].text == "1" and time.time() - start_time > ALONE_TIMEOUT:
             return True
     except:
         pass
-    
+
     try:
-        participants = driver.find_elements(
-            By.XPATH, "/html/body/div[1]/c-wiz/div[1]/div/div[13]/div[3]/div[10]/div/div/div[3]/div[1]/div[2]/div/div/div")
-        if participants and participants[0].text == "1"  and time.time() - start_time > ALONE_TIMEOUT:
+        participants = driver.find_elements(By.XPATH, Locators.MEET_ROOM_PARTICIPANTS_ONE)
+        if participants and participants[0].text == "1" and time.time() - start_time > ALONE_TIMEOUT:
             return True
     except:
         pass
-    
+
     try:
-        participants = driver.find_elements(
-            By.XPATH, "/html/body/div[1]/c-wiz/div[1]/div/div[13]/div[3]/div[11]/div/div/div[3]/div[1]/div[2]/div/div/div")
-        if participants and participants[0].text == "1"  and time.time() - start_time > ALONE_TIMEOUT:
+        participants = driver.find_elements(By.XPATH, Locators.MEET_ROOM_PARTICIPANTS_TWO)
+        if participants and participants[0].text == "1" and time.time() - start_time > ALONE_TIMEOUT:
             return True
     except:
         pass
-    
+
     try:
-        participants = driver.find_elements(
-            By.XPATH, "/html/body/div[1]/c-wiz/div[1]/div/div[14]/div[3]/div[10]/div/div/div[3]/div[1]/div[2]/div/div/div")
-        if participants and participants[0].text == "1"  and time.time() - start_time > ALONE_TIMEOUT:
+        participants = driver.find_elements(By.XPATH, Locators.MEET_ROOM_PARTICIPANTS_THREE)
+        if participants and participants[0].text == "1" and time.time() - start_time > ALONE_TIMEOUT:
             return True
     except:
         pass
     try:
-        participants = driver.find_elements(
-            By.XPATH, "/html/body/div[1]/c-wiz/div[1]/div/div[14]/div[3]/div[11]/div/div/div[3]/div[1]/div[2]/div/div/div")
-        if participants and participants[0].text == "1"  and time.time() - start_time > ALONE_TIMEOUT:
+        participants = driver.find_elements(By.XPATH, Locators.MEET_ROOM_PARTICIPANTS_FOUR)
+        if participants and participants[0].text == "1" and time.time() - start_time > ALONE_TIMEOUT:
             return True
     except:
         pass
 
     return False
-
 
 
 def clickButton(by, name):
@@ -183,10 +187,10 @@ def clickButton(by, name):
         waitwithss(1)
     except Exception as e:
         print(f"Failed to click button with {by} - {name}")
-        
+
 
 def watchMutation():
-  driver.execute_script("""
+    driver.execute_script("""
         function waitForElm(selector) {
             return new Promise(resolve => {
                 if (document.querySelector(selector)) {
@@ -225,7 +229,7 @@ def watchMutation():
         function switchStr(st, speaker_name) {
             var timestamp = (Math.ceil(Date.now()/1000)).toString()
             speaker_content +=  timestamp + "==>" + speaker_name + ";;;";
-            
+
         }
 
         var current_video = null;
@@ -246,38 +250,48 @@ def watchMutation():
         }, 500)
 """, int(time.time()))
 
+
 def run_bot():
-
     link = os.getenv("MEETING_LINK")
-    driver.get(link)
-    waitwithss(2)
+    print("Your Link: " + link)
+    if link is not None:
+        driver.get(link)
+    # The rest of your code
+    else:
+        print("MEETING_LINK environment variable is not set.")
+    waitwithss(3)
     action = EnhancedActionChains(driver)
+    #
+    # # bot_email = os.getenv("BOT_LOGIN_EMAIL")
+    # # bot_password = os.getenv("BOT_LOGIN_PASSWORD")
+    # # Start Login process otherwise cannot click continue :D
+    # # send_status("LOGGING_IN")
+    # check_if_detected(driver)
 
-    bot_email = os.getenv("BOT_LOGIN_EMAIL")
-    bot_password = os.getenv("BOT_LOGIN_PASSWORD")
-    # Start Login process otherwise cannot click continue :D
-    send_status("LOGGING_IN")
-    check_if_detected(driver)
-    driver.find_element(By.XPATH, "//*[contains(text(),'Sign in')]").click()
-    waitwithss(5)
-    
-    # Back to join screen
-    check_if_detected(driver)
-    driver.get(link)
-    waitwithss(2)
     try:
-        driver.find_element(By.XPATH, "//*[contains(text(),'Use without an account')]").click()
-        waitwithss(2)
-    except:
-        pass
-    action.send_keys_1by1("Wudpecker.io Notetaker").perform()
+        name_input = driver.find_element(By.CSS_SELECTOR, Locators.MEET_NAME_INPUT_FIELD)
+        name_input.send_keys("abm")
+    except Exception as e:
+        print("No 'Your name' input field found. Skipping...")
+    waitwithss(10)
+
+    # # Back to join screen
+    # check_if_detected(driver)
+    # driver.get(link)
+    # waitwithss(2)
+    # try:
+    #     driver.find_element(By.XPATH, "//*[contains(text(),'Use without an account')]").click()
+    #     waitwithss(2)
+    # except:
+    #     pass
+    # action.send_keys_1by1("Wudpecker.io Notetaker").perform()
     # Try to join
     check_if_detected(driver)
-    ask_to_join = driver.find_elements(By.XPATH, "//*[contains(text(),'Ask to join')]")
+    ask_to_join = driver.find_elements(By.XPATH, Locators.MEET_ASK_TO_JOIN_BUTTON)
     if ask_to_join:
-        driver.find_element(By.XPATH, "//*[contains(text(),'Ask to join')]").click()
+        driver.find_element(By.XPATH, Locators.MEET_ASK_TO_JOIN_BUTTON).click()
     else:
-        driver.find_element(By.XPATH, "//*[contains(text(),'Join now')]").click()
+        driver.find_element(By.XPATH, Locators.MEET_ASK_TO_JOIN_BUTTON).click()
     waitwithss(1)
 
     # Waiting in Lobby
@@ -300,12 +314,14 @@ def run_bot():
     watchMutation()
     with open('start.txt', 'w') as f:
         f.write('Bot joined')
+        print("Bot joined")
     while True:
         if alone_in_room(driver, start_time):
             send_status("EXIT_ALONE")
             break
         if is_kicked(driver):
             send_status("EXIT_KICKED")
+            print("Kicked out from the call.")
             break
         if timed_out(driver, start_time):
             send_status("EXIT_TIMEOUT")
@@ -314,15 +330,15 @@ def run_bot():
 
     # Call ended
     stopStream()
-    print("Call ended successfully",flush=True)
-
+    print("Call ended successfully", flush=True)
 
     waitwithss(2)
 
+
 try:
-    print('STARTING_BOT',flush=True)
+    print('STARTING_BOT', flush=True)
     run_bot()
 except Exception as e:
-    print(e,flush=True)
-    print('FAILED',flush=True)
+    print(e, flush=True)
+    print('FAILED', flush=True)
     send_status("FAILED")
